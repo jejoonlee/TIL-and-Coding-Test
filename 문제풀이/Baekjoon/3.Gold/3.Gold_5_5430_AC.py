914e4d0e15ab5fe53fe126378ab1d6e14d9e0a5f
jejoonlee <jejoonlee1996@gmail.com>
2022-09-08 22:39:46 +0900

    Add Baekjoon 20220908

--- 문제풀이/Baekjoon/3.Gold/3.Gold_5_5430_AC.py
+++ 문제풀이/Baekjoon/3.Gold/3.Gold_5_5430_AC.py
@@ -6,29 +6,45 @@ from collections import deque
 T = int(input())
 
 for _ in range(T):
-    command = input()
-    N = int(input())
-    array = input()
+    p = input()
+    n = int(input())
+    array = input().rstrip()[1:-1].split(',')
+    cnt = 0
     flag = True
 
-    # ','로 split 되어 있는 것을 리스트 안에 넣는다
-    array = deque(array[1:-1].split(','))
+    queue = deque(array)
 
-    for c in command:
+    if n == 0:
         queue = []
-        if c == 'R':
-            while len(array) != 0:
-                queue.append(array.pop())
-            for i in queue:
-                array.append(i)
-        
-        elif c == 'D':
-            if array:
-                array.popleft()
-            else:
-                flag = False
-                print('error')
 
-    if flag != False:
-        array = list(map(int, array))
-        print(array)
+    for command in p:
+        # 뒤집어지는 기준은 홀수는 배열이 뒤집어 지고
+        # 짝수는 기존 배열이랑 똑같다
+        if command == 'R':
+            if queue:
+                cnt += 1
+        
+        elif command == 'D':
+            # array에 숫자가 있으면
+            if queue:
+                # 뒤집어져 있을 때에는 맨 뒤에 있는 숫자를 pop한다
+                if cnt % 2 == 1:
+                    queue.pop()
+                
+                # 원상태이면 맨 앞의 숫자를 pop한다
+                elif cnt % 2 == 0:
+                    queue.popleft()
+            
+            else:
+                print('error')
+                flag = False
+                break
+    
+    if flag == True:
+        if cnt % 2 == 1:
+            queue.reverse()
+            queue = ','.join(queue)
+            print(f'[{queue}]')
+        else:
+            queue = ','.join(queue)
+            print(f'[{queue}]')
