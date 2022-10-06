0b9087fb85e82d1c7bfc40b1729616971010429f
jejoonlee <jejoonlee1996@gmail.com>
2022-10-06 22:03:20 +0900

    update 20221006

--- 문제풀이/Baekjoon/2.Silver/2.Silver_1_13335_트럭.py
+++ 문제풀이/Baekjoon/2.Silver/2.Silver_1_13335_트럭.py
@@ -12,38 +12,31 @@ cnt = 0
 weight = 0
 
 while True:
-  if trucks:
-    truck = trucks.popleft()
-    weight += truck
+  weight += trucks[0]
 
-    if weight <= L:
-      queue.popleft()
-      queue.append(truck)
-      cnt += 1
-    
-    else:
-      while True:
-        queue.popleft()
-        queue.append(0)
-        cnt += 1
+  if weight <= L:
+    queue.append(trucks[0])
+    queue.popleft()
+    trucks.popleft()
 
-        if sum(queue) + truck <= L:
-          queue.popleft()
-          queue.append(truck)
-          weight = 0
-          break
-  
   else:
     while True:
-      queue.popleft()
+      out = queue.popleft()
       queue.append(0)
-      cnt += 1
 
-      if sum(queue) == 0:
-        queue.popleft()
-        queue.append(0)
-        weight = 0
-        break
-    break
+      if trucks:
+        if sum(queue) + trucks[0] > L:
+          queue.popleft()
+          queue.append(0)
+        
+        else:
+          if weight - out + trucks[0] <= L:
+            weight = weight - out
+            break
+
+
+
+  
+
 
 print(cnt)
---
+++ 문제풀이/Baekjoon/3.Gold/3.Gold_5_10026_적록색약.py
@@ -0,0 +1,84 @@
+import sys
+sys.stdin = open('input.txt')
+
+# 적록색약은 빨간색과 초록색을 한 색깔로 같이 본다
+
+def pprint(lst):
+  for i in lst:
+    print(i)
+
+N = int(input())
+colors = [list(input()) for _ in range(N)]
+
+dr = [-1, 0, 0, 1]
+dc = [0, -1, 1, 0]
+
+def dfs(row, col):
+  stack = [(row, col)]
+
+  while stack:
+    now = stack.pop()
+
+    for i in range(4):
+      sr = dr[i] + now[0]
+      sc = dc[i] + now[1]
+
+      if 0 <= sr < N and 0 <= sc < N:
+        if colors[sr][sc] == color:
+          if color == 'B':
+            colors[sr][sc] = 'v'
+            stack.append((sr,sc))
+          elif color == 'G' or color == 'R':
+            colors[sr][sc] = 'h'
+            stack.append((sr,sc))
+          elif color == 'h':
+            colors[sr][sc] = 'visited'
+            stack.append((sr,sc))
+          elif color == 'v':
+            colors[sr][sc] = 'visited'
+            stack.append((sr,sc))
+
+cnt = [0, 0]
+
+
+for row in range(N):
+  for col in range(N):
+    
+    if colors[row][col] == 'B':
+      color = 'B'
+      colors[row][col] = 'v'
+      cnt[0] += 1
+
+      dfs(row,col)
+
+    elif colors[row][col] == 'R':
+      color = 'R'
+      colors[row][col] = 'h'
+      cnt[0] += 1
+
+      dfs(row, col)
+
+    elif colors[row][col] == 'G':
+      color = 'G'
+      colors[row][col] = 'h'
+      cnt[0] += 1
+
+      dfs(row, col)
+
+for row in range(N):
+  for col in range(N):
+    if colors[row][col] == 'v':
+      color = 'v'
+      colors[row][col] = 'visited'
+      cnt[1] += 1
+
+      dfs(row, col)
+
+    elif colors[row][col] == 'h':
+      color = 'h'
+      colors[row][col] = 'visited'
+      cnt[1] += 1
+
+      dfs(row, col)
+
+print(' '.join(map(str, cnt)))
