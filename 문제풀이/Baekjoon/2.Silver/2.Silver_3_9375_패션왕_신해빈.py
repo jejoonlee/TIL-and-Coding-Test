e5a28c23702fa31ddc9d44f91ed4e1c84a195fc6
jejoonlee <jejoonlee1996@gmail.com>
2022-10-13 22:07:23 +0900

    마지막 20221013 없데이트

--- 문제풀이/Baekjoon/2.Silver/2.Silver_3_9375_패션왕_신해빈.py
+++ 문제풀이/Baekjoon/2.Silver/2.Silver_3_9375_패션왕_신해빈.py
@@ -1,16 +1,28 @@
 import sys
 sys.stdin = open('input.txt')
 
-T = int(input())
+# 수학
+# (종류 + 1) * (종류 + 1)..... -1 
+# -1 은 아무것도 착용을 안 했을 때에
+# +1 은 해당 종류를 입어도 되고, 안 입어도 되니깐
 
+T = int(input())
 
 for _ in range(T):
   N = int(input())
-  clothes = []
+  clothes = {}
   for n in range(N):
     A, B = input().split()
-    clothes.append((A,B))
+    
+    if B in clothes:
+      clothes[B] += 1
+    
+    elif B not in clothes:
+      clothes[B] = 1
+      
+  result = 1
 
-  
+  for cloth in clothes:
+    result *= (clothes[cloth] + 1)
 
-  print(clothes)
+  print(result - 1)
