50ba49206563ff217e02b10ff25286756847eabf
jejoonlee <jejoonlee1996@gmail.com>
2022-10-17 22:17:29 +0900

    accounts 필드에 profile_pic 넣기

--- Python/Django/Practice/20221017/accounts/forms.py
+++ Python/Django/Practice/20221017/accounts/forms.py
@@ -4,4 +4,4 @@ from django.contrib.auth.forms import UserCreationForm
 class UserCustomForm(UserCreationForm):
   class Meta:
     model = get_user_model()
-    fields = ['username', 'password1', 'password2', 'email', 'first_name', 'last_name', 'gender']
+    fields = ['username', 'password1', 'password2', 'email', 'first_name', 'last_name', 'gender', 'profile_pic']
