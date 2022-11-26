d7fce7e25fc71a82d81cb2fcb00a40a489a69b6e
jejoonlee <jejoonlee1996@gmail.com>
2022-11-26 21:58:59 +0900

    크롤링 조사

---
+++ Python/Django/Practice/crawling/crawling_test/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CrawlingTestConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'crawling_test'
---
+++ Python/Django/Practice/crawling/crawling_test/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+
+app_name = 'crawling_test'
+
+urlpatterns = [
+]
