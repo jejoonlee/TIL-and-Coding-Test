672ba4de5e72e72872a2737e8746b568b9a7d8e1
jejoonlee <jejoonlee1996@gmail.com>
2023-01-01 15:55:48 +0900

    백준 1744 문제풀이

--- 문제풀이/Baekjoon/3.Gold/3.Gold_4_1744_수_묶기.py
+++ 문제풀이/Baekjoon/3.Gold/3.Gold_4_1744_수_묶기.py
@@ -1,31 +1,45 @@
 import sys
-sys.stdin = open('input.txt')
+
+sys.stdin = open("input.txt")
 from collections import deque
 
 N = int(input())
-array = []
+pos_array = []
+nav_array = []
 
 for _ in range(N):
-    array.append(int(input()))
+    num = int(input())
+    if num > 0:
+        pos_array.append(num)
+    else:
+        nav_array.append(num)
 
-array.sort(key=lambda x: -x)
-array = deque(array)
+pos_array.sort(key=lambda x: -x)
+nav_array.sort()
+pos_array = deque(pos_array)
+nav_array = deque(nav_array)
 
-ans = 0
+pos_ans = 0
+nav_ans = 0
 
-while len(array) != 0 :
-    if array[1]:
-        a = array.popleft()
-        b = array.popleft()
-        
-        if a * b > 0:
-            ans += a * b
-        elif a * b < 0:
-            ans += a + b
-        else:
-            if a + b > 0:
-                ans += a + b
-            elif a + b < 0:
-                ans += a* b
+while len(pos_array) != 0:
+    if len(pos_array) != 1:
+        a = pos_array.popleft()
+        b = pos_array.popleft()
+        if a != 1 and b != 1:
+            pos_ans += a * b
+        elif a == 1 or b == 1:
+            pos_ans += a + b
+    else:
+        a = pos_array.popleft()
+        pos_ans += a
 
-print(ans)
+while len(nav_array) != 0:
+    if len(nav_array) != 1:
+        a = nav_array.popleft()
+        b = nav_array.popleft()
+        nav_ans += a * b
+    else:
+        nav_ans += nav_array.popleft()
+
+print(pos_ans + nav_ans)
