aef395b15af409e356f7fd3863dc361ddf4c3c37
jejoonlee <jejoonlee1996@gmail.com>
2023-02-08 10:48:51 +0900

    udemy python 코드

---
+++ 블로그/Python/연습/day_38_my_workout/main.py
@@ -0,0 +1,51 @@
+import requests
+import os
+import dotenv
+from datetime import datetime
+
+dotenv.load_dotenv()
+
+endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheet_endpoint = os.getenv("sheet_endpoint")
+
+header = {
+    "x-app-id" : os.getenv("APPID"),
+    "x-app-key" : os.getenv("nutritionix_api_key"),
+}
+
+exercise = input("Tell me which exercises you did: ").lower()
+
+parameter = {
+    "query": exercise,
+    "gender" : "MALE",
+    "age" : 27,
+    "height_cm" : 170,
+    "weight_kg" : 70,
+}
+
+response = requests.post(url=endpoint, json=parameter, headers=header)
+response.raise_for_status()
+
+workout_list = response.json()['exercises']
+
+
+now = datetime.now()
+
+now_date = now.strftime("%Y/%m/%d")
+now_time = now.strftime("%X")
+
+
+for workout in workout_list:
+    sheet_input = {
+        "workout": {
+            "date": now_date,
+            "time": now_time,
+            "exercise": workout["name"].title(),
+            "duration": int(workout["duration_min"]),
+            "calories": workout["nf_calories"],
+        }  
+    }
+
+    sheet_response = requests.post(url=sheet_endpoint, json=sheet_input)
+
+    print(sheet_response.text)
