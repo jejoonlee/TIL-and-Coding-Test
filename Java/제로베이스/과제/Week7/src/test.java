771c442c5a0e6a4cd373db514a90f80e724e1f11
jejoonlee <jejoonlee1996@gmail.com>
2023-08-20 18:52:14 +0900

    20230820

--- Java/제로베이스/과제/Week7/src/test.java
+++ Java/제로베이스/과제/Week7/src/test.java
@@ -1,73 +1,73 @@
 import java.util.*;
 
-public class test {
-    static int[] arrA;
-    static int[] arrB;
-    static List<Integer> list = new ArrayList<>();
-    public static void main(String[] args) {
-        int[] a = {1};
-        int[] b = {9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9};
-        arrA = a.clone();
-        arrB = b.clone();
+class Solution {
+    public static int solution(String[] ingredients, String[] items) {
+        int answer = 0;
+        int left = 0;
+        int right = items.length - 1;
 
-        soltuion(a.length - 1, b.length - 1);
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
 
-        int[] answer = new int[list.size()];
-        int index = list.size() - 1;
-        for (int i = 0 ; i < answer.length; i++) {
-            answer[i] = list.get(index--);
+        for (int i = 0; i < ingredients.length; i++) {
+            if (map.containsKey(ingredients[i])) {
+                int num = map.get(ingredients[i]);
+                map.put(ingredients[i], num + 1);
+            } else {
+                map.put(ingredients[i], 1);
+            }
         }
-        System.out.println(answer);
+
+        while (true) {
+            if (!map.containsKey(items[left]) && map.containsKey(items[right])) {
+                left++;
+            } else if (map.containsKey(items[left]) && !map.containsKey(items[right])) {
+                right--;
+            } else if (!map.containsKey(items[left]) && !map.containsKey(items[right])) {
+                left++;
+                right--;
+            } else {
+                HashMap<String, Integer> tempMap = new HashMap<String, Integer>();
+
+                for (int i = left; i <= right; i++) {
+                    if (map.containsKey(items[i]) && tempMap.containsKey(items[i])) {
+                        int num = tempMap.get(items[i]);
+                        tempMap.put(items[i], num + 1);
+                    } else if (map.containsKey(items[i]) && !tempMap.containsKey(items[i])) {
+                        tempMap.put(items[i], 1);
+                    }
+                }
+
+                ArrayList<String> extra = new ArrayList<>();
+                boolean isAnswer = true;
+
+                for (String key : map.keySet()) {
+                    if (map.get(key) < tempMap.get(key)) {
+                        isAnswer = false;
+                        extra.add(key);
+                    }
+                }
+
+                if (isAnswer) {
+                    answer = right - left + 1;
+                    break;
+                } else {
+                    if (extra.contains(items[left])) {
+                        left++;
+                    } else if (extra.contains(items[right])) {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return answer;
     }
 
-    private static void soltuion(int aIdx ,int bIdx) {
-        // 한쪽만 종료된 경우 1
-        if (aIdx < 0 && bIdx >= 0) {
-            // 한쪽이 10이 넘는 경우가 있음 그러면 자릿 수 올리기 또는 그냥 나눈 값 넣기
-            if (arrB[bIdx] >= 10) {
-                list.add(arrB[bIdx] % 10);
-                if (bIdx - 1 >= 0) {
-                    arrB[bIdx - 1] +=1;
-                }else {
-                    list.add(arrB[bIdx] / 10);
-                }
-            } else {
-                list.add(arrB[bIdx]);
-            }
-            soltuion(aIdx,--bIdx);
-            return;
-        }
-        // 한쪽만 종료된 경우 2
-        else if (aIdx >= 0 && bIdx < 0) {
-            if (arrA[aIdx] >= 10) {
-                list.add(arrA[aIdx] % 10);
-                if (aIdx - 1 >= 0) {
-                    arrA[aIdx - 1] += 1;
-                } else {
-                    list.add(arrA[aIdx] / 10);
-                }
-            } else {
-                list.add(arrA[aIdx]);
-            }
-            soltuion(--aIdx,bIdx);
-            return;
-        } else if (aIdx < 0 && bIdx < 0) { // 둘다 0 이하인 경우
-            return;
-        }
+    public static void main(String[] args) {
+        String[] ingredients = {"생닭", "인삼", "소주", "대추"};
+        String[] items = {"물", "인삼", "커피", "생닭", "소주", "사탕", "생닭", "대추", "쌀"};
 
-        // 두 개의 index가 0이상인 경우 아래의 재귀를 수행
-        if (arrA[aIdx] + arrB[bIdx] >= 10) {
-            list.add((arrA[aIdx] + arrB[bIdx]) % 10);
-
-            if (aIdx - 1 >= 0) {
-                arrA[aIdx - 1] += 1;
-            } else if (bIdx - 1 >= 0) {
-                arrB[bIdx - 1] += 1;
-            }else if (aIdx == 0 || bIdx == 0) list.add(1);
-        } else {
-            list.add(arrA[aIdx] + arrB[bIdx]);
-        }
-
-        soltuion(--aIdx, --bIdx);
+        int result = solution(ingredients, items);
+        System.out.println("Maximized Value: " + result);
     }
 }
